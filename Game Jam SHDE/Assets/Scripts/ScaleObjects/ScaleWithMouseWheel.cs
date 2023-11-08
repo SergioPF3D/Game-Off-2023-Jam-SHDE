5077f1468445d59cbd13cdf1702736b3c8bf14c3
hugoDelgado-02 <hugo.delgado.delgallo@gmail.com>
2023-11-08 13:54:15 +0100

    asd

--- Game Jam SHDE/Assets/Scripts/ScaleObjects/ScaleWithMouseWheel.cs
+++ Game Jam SHDE/Assets/Scripts/ScaleObjects/ScaleWithMouseWheel.cs
@@ -22,7 +22,7 @@ public class ScaleWithMouseWheel : ScalableObject
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
             //Scale up
-            if (transform.localScale.x + Input.GetAxis("Mouse ScrollWheel") <= maxScale || transform.localScale.y + Input.GetAxis("Mouse ScrollWheel") <= maxScale || transform.localScale.z + Input.GetAxis("Mouse ScrollWheel") <= maxScale)
+            if (transform.localScale.x + Input.GetAxis("Mouse ScrollWheel") * scaleSpeed.x <= maxScale && transform.localScale.y + Input.GetAxis("Mouse ScrollWheel") * scaleSpeed.y <= maxScale && transform.localScale.z + Input.GetAxis("Mouse ScrollWheel") * scaleSpeed.z <= maxScale)
             {
                 transform.localScale += Input.GetAxis("Mouse ScrollWheel") * scaleSpeed;
                 this.GetComponent<Rigidbody>().mass = (this.transform.localScale.x + this.transform.localScale.y + this.transform.localScale.z) / 3;
@@ -31,7 +31,7 @@ public class ScaleWithMouseWheel : ScalableObject
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
             //Scale down
-            if (transform.localScale.x + Input.GetAxis("Mouse ScrollWheel") >= minScale || transform.localScale.y + Input.GetAxis("Mouse ScrollWheel") >= minScale || transform.localScale.z + Input.GetAxis("Mouse ScrollWheel") >= minScale)
+            if (transform.localScale.x + Input.GetAxis("Mouse ScrollWheel") * scaleSpeed.x >= minScale && transform.localScale.y + Input.GetAxis("Mouse ScrollWheel") * scaleSpeed.y >= minScale && transform.localScale.z + Input.GetAxis("Mouse ScrollWheel") * scaleSpeed.z >= minScale)
             {
                 transform.localScale += Input.GetAxis("Mouse ScrollWheel") * scaleSpeed;
                 this.GetComponent<Rigidbody>().mass = (this.transform.localScale.x + this.transform.localScale.y + this.transform.localScale.z) / 3;
